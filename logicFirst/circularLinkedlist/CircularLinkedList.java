c925a17107beeb246176dc0e296ba1d59a9e7d9c
Jobi S S <108945660+jobissjo@users.noreply.github.com>
2023-07-31 06:14:15 +0000

    LinkedList problem is completed

--- logicFirst/circularLinkedlist/CircularLinkedList.java
+++ logicFirst/circularLinkedlist/CircularLinkedList.java
@@ -1,3 +1,6 @@
+import java.lang.IndexOutOfBoundsException;
+import java.lang.RuntimeException;
+
 public class CircularLinkedList<T>{
     Node last;
 
@@ -48,13 +51,126 @@ public class CircularLinkedList<T>{
             return;
         }
         Node temp = last.next;
-
-
         do{
             System.out.print(temp.data + " ");
             temp = temp.next;
-        } while(temp.next != last);
+        } while(temp != last.next);
         System.out.println();
     }
+    public void deleteAtBegining(){
+        if (last == null)
+            nullChecker();
+        if (last.next == last){
+            last = null;
+        }
+        else{
+            last.next = last.next.next;
+        }
+    }
+
+    public void deleteAtEnd(){
+        if (last == null){
+            nullChecker();
+        }
+        if (last.next == last){
+            last = null;
+            return;
+        }
+        
+        else{
+            Node temp = last.next;
+            while (temp == last){
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+            last = temp;
+        }
+    }
+    public void deleteVal(T val){
+        if (last == null){
+            nullChecker();
+        }
+        else if (last.next == last){
+            if (last.data.equals(val))
+                last = null;
+            return;
+        }
+        else{
+            Node temp = last.next;
+            Node prev = last;
+            do{
+                if (temp.data.equals(val)){
+                    if (last == temp)
+                        last = prev;
+                    prev.next = temp.next;
+                    return;   
+                }
+                prev = temp;
+                temp = temp.next;
+            } while(temp != last.next);
+            System.out.println("No items Found");
+        }
+    }
+
+    public void insertAfterVal(T exVal, T val){
+        if (last == null){
+            nullChecker();
+        }
+        else{
+            Node temp = last.next;
+            Node newNode = new Node(val);
+            do{
+                if (temp.data.equals(exVal)){
+                    if (last == temp)
+                        last = newNode;
+                    newNode.next = temp.next;
+                    temp.next = newNode;
+                    return;
+                }
+                temp = temp.next;
+            } while(temp != last.next);
+            throw new ValueNotFoundException(exVal+" is not found in list"); 
+        }
+    }
+
+    public boolean contains(T val){
+        boolean isContain = false;
+        if(last == null)
+            nullChecker();
+        Node temp = last.next;
+        do{
+            if (temp.data.equals(val))
+                isContain = true;
+            temp = temp.next;
+        }while(temp != last.next);
+        return isContain;
+    }
+
+    private void nullChecker(){
+        throw new IndexOutOfBoundsException("List is empty");
+    }
+    public int size(){
+        if(last == null)
+            return 0;
+
+        Node temp = last.next;
+        int count = 0;
+        do{
+            temp = temp.next;
+            count++;
+        } while(temp != last.next);
+        
+        return count;
+    }
+
+    
     
 }
+class ValueNotFoundException extends RuntimeException {
+    public ValueNotFoundException(String message) {
+        super(message);
+    }
+}
+
+
+
--- logicFirst/circularLinkedlist/Main.java
+++ logicFirst/circularLinkedlist/Main.java
@@ -6,5 +6,33 @@ public class Main{
         list.insertAtBegining("joy");
         list.insertAtEnd("Jobin");
         list.display();
+        list.deleteAtBegining();
+        list.display();
+        list.deleteAtEnd();
+        list.deleteAtBegining();
+        System.out.println("no element");
+        list.display();
+        // deleteVal
+        list.insertAtBegining("jobi");
+        list.insertAtBegining("hello");
+        list.insertAtBegining("Hii");
+        list.deleteVal("jobi");
+        list.deleteVal("Hii");
+        // list.deleteVal("hello");
+        list.display();
+
+
+        // insertAfter("somval")
+        list.insertAfterVal("hello", "man");
+        list.insertAfterVal("hello", "jobi");
+        list.display();
+        // list.insertAfterVal("man", "sds");
+        // list.insertAfterVal("jobi", "sdjl");
+        list.display();
+        // size
+        System.out.println(list.size());
+
+        // contains
+        System.out.println(list.contains("hello"));
     }
 }
